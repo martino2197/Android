30cb651ccaaea1ad0770480ffd760cdc1a7dafa1
martino2197 <martino2197@gmail.com>
2020-01-23 13:09:21 -0600

    Kotlin Basico

---
+++ Kotlin/src/HolaMundo.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    println("Hola Mundo")
+    //Operadores En Kotlin
+    println(1+1)
+    println(3-1)
+    println(2*2)
+    println(4/2)
+    println(7%2)
+    println("\n")
+    val a = 4
+    val b = 2
+
+    println(a.plus(b))
+    println(a.minus(b))
+    println(a.times(b))
+    println(a.div(b))
+    //TODO EN KOTLIN ES UN OBJETO
+}
+
+//click derecho y le damos a la opcion de Run
