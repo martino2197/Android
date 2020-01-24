d809207486cd5a0a41069eff6ea834673d1958f2
martino2197 <martino2197@gmail.com>
2020-01-24 12:02:42 -0600

    Strings: .trimIdent() y .trimMargin()

--- Kotlin/src/HolaMundo.kt
+++ Kotlin/src/HolaMundo.kt
@@ -33,6 +33,35 @@ fun main(args: Array<String>) {
 
     println(name)
     println(N)
+    println("\n")
+
+    //STRINGS
+    val nombre = "Luis Martin"
+    val apellido: String = "Maceda Nazario"
+
+    val nombreapellido = "Martin\nMaceda"
+
+    //println("Tu nombre es: " + nombre)
+    println("Tu nombre es: $nombre $apellido")
+    //Raw String
+    val parrafo = """
+        **Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. 
+        **Lorem Ipsum has been the industry's standard dummy text
+        ever since the 1500s, when an unknown printer took a 
+        galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap
+        into electronic typesetting, remaining essentially unchanged. 
+        **
+        It was popularised in the 1960s with the release of Letraset 
+        sheets containing Lorem Ipsum passages, and more recently  
+        with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+    """.trimIndent() //este metodo respeta la indentacion de mi parrafo
+
+    println(parrafo.trimMargin("**"))
+
+    println("Tu nombre es: $nombreapellido")
 }
 
 //click derecho y le damos a la opcion de Run
