1a7fcbdd96917692a9d006f76cca003db10a8e9b
martino2197 <martino2197@gmail.com>
2020-01-24 11:24:49 -0600

    Tipos de variables: var, val y const

--- Kotlin/src/HolaMundo.kt
+++ Kotlin/src/HolaMundo.kt
@@ -1,4 +1,11 @@
+const val N = "NAME" //const tiempo de compilacion y en variables globales
+//val n = 3
+//var n = 3 //mala practica de programacion
+
+
+//Funciones Puras
 fun main(args: Array<String>) {
+
     println("Hola Mundo")
     //Operadores En Kotlin, valores primitivos vs objetos.
     println(1+1)
@@ -15,8 +22,17 @@ fun main(args: Array<String>) {
     println(a.minus(b))
     println(a.times(b))
     println(a.div(b))
+    println("\n")
 
     //TODO EN KOTLIN ES UN OBJETO
+
+    //a = 10 //val cannot be reassigned
+
+    //n = 10
+    val name = args[0] //tiempo de ejecucion, variables locales
+
+    println(name)
+    println(N)
 }
 
 //click derecho y le damos a la opcion de Run
